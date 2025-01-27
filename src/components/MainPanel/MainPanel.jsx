285399f08020a9dbce4cf42c581492c16ac3e9ed
Siddharth Kamboj <71596674+Siddharth2001-July@users.noreply.github.com>
2025-01-27 11:30:22 +0530

    sidebar and viewer, slider remaining

--- src/components/MainPanel/MainPanel.jsx
+++ src/components/MainPanel/MainPanel.jsx
@@ -1,27 +1,32 @@
-import { Panel } from "@baseline-ui/core";
+import { Panel, Text } from "@baseline-ui/core";
 import PdfViewerComponent from "../PdfViewerComponent";
 import PropTypes from "prop-types";
 import TopBar from "./TopBar";
 const MainPanel = ({ uploadedFile, onTextExtracted, selectedDocumentId, documents }) => {
   console.log('MainPanel uploadedFile:', uploadedFile);
   console.log('MainPanel uploadedFile type:', uploadedFile instanceof File, typeof uploadedFile);
-  
+
   return (
     <Panel order={2} defaultSize={60} minSize={40} className="main-panel">
-      <TopBar 
-        documents = {documents}
+      <TopBar
+        documents={documents}
         selectedDocumentId={selectedDocumentId}
       />
-      {uploadedFile ? (
-        <PdfViewerComponent
-          document={uploadedFile}
-          onTextExtracted={onTextExtracted}
-        />
-      ) : (
-        <div style={{ marginTop: "45%", fontWeight: 800, textAlign: "center" }}>
-          Upload a new document or select from the existing ones.
+      <div className="viewer-cont">
+        <div className="viewer-doc-area">
+          {uploadedFile ? (
+            <PdfViewerComponent
+              document={uploadedFile}
+              onTextExtracted={onTextExtracted}
+            />
+          ) : (
+            <div className="no-doc-text">
+              <Text type="value" size="sm">Upload a new document or select from the existing ones.</Text>
+            </div>
+          )}
         </div>
-      )}
+      </div>
+
     </Panel>
   );
 };
